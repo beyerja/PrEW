f63bb12cb78cad3f858224ec3f6e2ce2ad13b1de
Jakob Beyer <beyerjac@flc02.desy.de>
2019-11-27 17:54:58 +0100

    Make FitPar tests fatal
    - Switch EXPECT to ASSERT in FitPar tests to make them fatal instead of 
    optional.

--- source/tests/test_FitPar.cpp
+++ source/tests/test_FitPar.cpp
@@ -9,17 +9,17 @@ using namespace PREW::Fit;
 TEST(TestFitPar, ReturnsCorrectIni) {
   FitPar fp1 (0.00055);
   FitPar fp2 (-200.5);
-  EXPECT_EQ(fp1.get_val_ini(), 0.00055);
-  EXPECT_EQ(fp1.m_val_mod, 0.00055);
-  EXPECT_EQ(fp2.get_val_ini(), -200.5);
-  EXPECT_EQ(fp2.m_val_mod, -200.5);
+  ASSERT_EQ(fp1.get_val_ini(), 0.00055);
+  ASSERT_EQ(fp1.m_val_mod, 0.00055);
+  ASSERT_EQ(fp2.get_val_ini(), -200.5);
+  ASSERT_EQ(fp2.m_val_mod, -200.5);
 }
 
 TEST(TestFitPar, ProperReset) {
   FitPar fp (4000.77);
   fp.m_val_mod = 20.5;
   fp.reset();
-  EXPECT_EQ(fp.m_val_mod, 4000.77);
+  ASSERT_EQ(fp.m_val_mod, 4000.77);
 }
 
 TEST(TestFitPar, ProperOStream) {
@@ -29,5 +29,5 @@ TEST(TestFitPar, ProperOStream) {
   buffer << fp;
   std::string result = buffer.str();
   std::string expected = "Fit Parameter: initial value: -30.27 -> modified value: 2";
-  EXPECT_STREQ(result.c_str(), expected.c_str());
+  ASSERT_STREQ(result.c_str(), expected.c_str());
 }
