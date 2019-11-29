2d8b0c8b6edb2e9b7fd9cdf7f9227afae5ebe6db
Jakob Beyer <beyerjac@flc02.desy.de>
2019-11-29 10:17:03 +0100

    Test chi-Squared for large bin number
    - Adapt test of ChiSqMinimizer constructor to test chi-sq calculation 
    with large numbers of bins.

--- source/tests/test_ChiSqMinimizer.cpp
+++ source/tests/test_ChiSqMinimizer.cpp
@@ -22,18 +22,20 @@ TEST(TestChiSqMinimizer, SimpleConstructor) {
   ASSERT_EQ(chi_sq_minimizer.get_chisq(), 1);
 }
 
-TEST(TestChiSqMinimizer, MultibinConstructor) {
-  // Test with two FitBin's that should give chisq=2
+TEST(TestChiSqMinimizer, ManyBinsConstructor) {
+  // Test with n FitBin's, range n from small to very large 
+  // (testing mainly calc_chisq)
   std::function<double()> trivial_prd = []() { return 0; }; // Prediction = 0
-  FitBin fb1 (1.0, 1.0, trivial_prd); // Measurement = Unc = 1
-  FitBin fb2 (-1.0, 1.0, trivial_prd); // Measurement -1, Unc = 1
-  std::vector<FitPar> fit_pars {}; 
-  std::vector<FitBin> fit_bins {fb1, fb2};
-  FitContainer container {};
-  container.m_fit_pars = fit_pars;
-  container.m_fit_bins = fit_bins;
-  ChiSqMinimizer chi_sq_minimizer (&container);
-  ASSERT_EQ(chi_sq_minimizer.get_chisq(), 2);
+  std::vector<int> n_bins = {20, 200, 2000, 20000, 200000};
+  for ( auto n : n_bins ) {
+    std::vector<FitPar> fit_pars {}; 
+    std::vector<FitBin> fit_bins (n, FitBin(1.0, 1.0, trivial_prd));
+    FitContainer container {};
+    container.m_fit_pars = fit_pars;
+    container.m_fit_bins = fit_bins;
+    ChiSqMinimizer chi_sq_minimizer (&container);
+    ASSERT_EQ(chi_sq_minimizer.get_chisq(), n);
+  }
 }
 
 TEST(TestChiSqMinimizer, MultiBinPlusFitParConstructor) {
