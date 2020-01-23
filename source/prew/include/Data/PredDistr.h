a853dbb7d8ec041c0efc57a82592a2208794835a
Jakob Beyer <beyerjac@flc02.desy.de>
2020-01-23 10:44:54 +0100

    New PredDistr struct
    - Add new PredDistr struct to store predicted distributions.

---
+++ source/prew/include/Data/PredDistr.h
@@ -0,0 +1,28 @@
+#ifndef LIBRARY_PREDDISTR_H
+#define LIBRARY_PREDDISTR_H
+
+#include <Data/DistrInfo.h>
+
+#include <vector>
+
+namespace PREW {
+namespace Data {
+  
+  struct PredDistr {
+    /** Class holding a predictions for differential distribution of a process
+        for one chirality.
+    **/
+    
+    DistrInfo m_info {}; // Info which identifies this distribution
+    
+    // Values of observables at bin center
+    CppUtils::Vec::Matrix2D<double> m_bin_centers {}; 
+    std::vector<double> m_distribution {}; // Predicted distribution
+  };
+  
+  typedef std::vector<PredDistr> PredDistrVec;
+  
+} 
+}
+
+#endif
