374b6e489faf1cf68bfabe91c25f5edb0feb9afa
Jakob Beyer <beyerjac@flc02.desy.de>
2020-01-27 17:23:20 +0100

    Correct documentation of DiffDistr
    - Correct the documentation in DiffDistr, explain why only number of 
    events can be used.

--- source/prew/include/Data/DiffDistr.h
+++ source/prew/include/Data/DiffDistr.h
@@ -12,8 +12,10 @@ namespace Data {
   struct DiffDistr {
     /** Class holding a differential distribution of a process for a 
         polarisation combination.
-        -> Save all (necessary) information directly with distribution
-           (Distributions with common arguments can be found easily)
+        Differential distributions can only be distributions of numbers of 
+        events in the context of this framework. Everything else (e.g cross 
+        section) would carry a systematic error that this framework is not made
+        to account for.
     **/
     
     DistrInfo m_info {}; // Info which identifies this distribution
@@ -22,7 +24,7 @@ namespace Data {
     // => Are not stored with the bin because underlying fit framework 
     //    doesn't need it
     CppUtils::Vec::Matrix2D<double> m_bin_centers {}; 
-    Fit::BinVec m_distribution {}; // Observable values (e.g. cross section)
+    Fit::BinVec m_distribution {}; // Distribution of number of events
   };
   
   typedef std::vector<DiffDistr> DiffDistrVec;
