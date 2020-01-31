3ca07266c2408a32a611cd61d14f0ba95364f395
Jakob Beyer <beyerjac@flc02.desy.de>
2020-01-31 11:40:54 +0100

    Add C++ helper namespace for maps
    - Add helper namespace for maps which (for now) only consists of 
    aliases.

---
+++ source/prew/include/CppUtils/Map.h
@@ -0,0 +1,25 @@
+#ifndef LIB_CPPHELPMAP_H
+#define LIB_CPPHELPMAP_H 1
+
+#include <map>
+#include <set>
+#include <vector>
+
+namespace PREW {
+namespace CppUtils {
+
+namespace Map {
+  using StrToStrVecMap  = std::map<std::string,std::vector<std::string>>;
+  using StrToStrPairMap = 
+    std::map<std::string, std::pair<std::string, std::string>>;
+  
+  using DoubleToStrVecMap = std::map<double, std::vector<std::string>>;
+  using IntToStrVecMap    = std::map<int, std::vector<std::string>>;
+  
+  using IntToStrSetMap = std::map<int, std::set<std::string>>;
+}
+
+}
+}
+
+#endif
