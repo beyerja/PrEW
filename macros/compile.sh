57bbdc4a176591049d5d1f088bfbd30e20049aad
beyerja <beyerja@gmx.de>
2020-02-11 16:37:40 +0100

    Allow multithreaded compilation
    - Change compilation macro so that different input arguments can be 
    supplied.
    - Add multithreaded mode to compilation macro.
    - Remark multithreaded compilation possibility in Readme.

--- macros/compile.sh
+++ macros/compile.sh
@@ -3,22 +3,54 @@
 # -------------------------------------------------#
 # Bash script for compiling the processor of the
 # analysis.
-# To be run simply by using: ./compile.sh [-rebuild]
+# To be run simply by using: ./compile.sh [--rebuild] [--jobs=n_jobs]
 # Be sure a modern software is sourced! (E.g. sourcing load_env.sh)
 # -------------------------------------------------#
 
-if [[  $# > 1 ]]  ; then
-	echo "usage: ./compile.sh [-rebuild]"
-	exit
-fi
-
 # -------------------------------------------------#
-# Optional input parameter:
-# Should everything be rebuild? 
-# (-> will delete content of build folder)
-if [ $# == 1 ]; then
-  REBUILD=true
-fi
+# Check for input arguments to script
+
+# Default parameters
+REBUILD=false
+N_JOBS=1
+
+# Help message
+USAGE="Usage: ./compile.sh [-h/--help/-H] [--rebuild] [--jobs=n_jobs]"
+
+for i in "$@"
+do
+case $i in
+  --jobs=*)
+    N_JOBS="${i#*=}"
+    echo "Main compilation will use ${N_JOBS} jobs."
+    shift # past argument=value
+  ;;
+  --rebuild)
+    REBUILD=true
+    shift
+  ;;
+  -h|--help|-H)
+    echo ""
+    echo "Macro to compile the PrEW source code"
+    echo "${USAGE}"
+    echo ""
+    echo "Arguments:"
+    echo "  -h/--help/-H  : Help information (this)." 
+    echo "  --rebuild     : Delete previous build folder for clean rebuild." 
+    echo "  --jobs=n_jobs : Set a number of cores to use in compilation." 
+    echo ""
+    exit
+  ;;
+  *)
+    # unknown option
+    echo ""
+    echo "Unknown argument: ${1}"
+    echo "${USAGE}"
+    echo ""
+    exit
+  ;;
+esac
+done
 
 # -------------------------------------------------#
 # Check if necessary directories are in place
@@ -29,7 +61,7 @@ home_folder=${dir}/..
 if [ -d "${home_folder}"/build  ] ; then
 	echo "Already have  --build-- directory to compile"
   # If requested remove build folder content
-  if [ ${REBUILD} ]; then 
+  if [ "${REBUILD}" = true ]; then 
     echo "Rebuild requested => Recreating build directory"
     rm -r "${home_folder}"/build
     mkdir "${home_folder}"/build
@@ -51,7 +83,7 @@ cmake ..
 wait 
 echo
 echo "begin to make" 
-make 
+make --jobs="${N_JOBS}"
 wait
 echo
 echo "begin to make install" 
