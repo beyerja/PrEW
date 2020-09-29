9206b2b94236cc3a1b4cc3060a8c64c72585ca11
beyerja <beyerja@gmx.de>
2020-09-29 15:25:16 +0200

    Add function to transform int chiralities into string
    - Add helper functions to transform e-e+ integer chiralities into the 
    corresponding PrEW string.

--- source/prew/include/GlobalVar/Chiral.h
+++ source/prew/include/GlobalVar/Chiral.h
@@ -1,24 +1,28 @@
 #ifndef LIBRARY_CHIRAL_H
 #define LIBRARY_CHIRAL_H 1
 
+// Standard library
 #include <string>
 
 namespace PrEW {
 namespace GlobalVar {
 /** Careful with this namespace, only for static const variables!
-**/
-    
-  namespace Chiral {
-    /** Common strings to mark generator level chiral distributions.
-    **/
-    
-    static const std::string eLpR = "PrEW-internal-GenLevel-ElectronL-PositronR";
-    static const std::string eRpL = "PrEW-internal-GenLevel-ElectronR-PositronL";
-    static const std::string eLpL = "PrEW-internal-GenLevel-ElectronL-PositronL";
-    static const std::string eRpR = "PrEW-internal-GenLevel-ElectronR-PositronR";
-  }
-    
-}
-}
+ **/
+
+namespace Chiral {
+/** Common strings to mark generator level chiral distributions.
+ **/
+
+static const std::string eLpR = "PrEW-internal-GenLevel-ElectronL-PositronR";
+static const std::string eRpL = "PrEW-internal-GenLevel-ElectronR-PositronL";
+static const std::string eLpL = "PrEW-internal-GenLevel-ElectronL-PositronL";
+static const std::string eRpR = "PrEW-internal-GenLevel-ElectronR-PositronR";
+
+std::string transform(int eM_chirality, int eP_chirality);
+
+} // namespace Chiral
+
+} // namespace GlobalVar
+} // namespace PrEW
 
 #endif
