076c1bf0f7ce3b11af1e1df19cece8de769ce3a0
beyerja <beyerja@gmx.de>
2020-10-19 17:05:23 +0200

    Use double comparison in BinCoord equal operator
    - Use double comparison in BinCoord equal operator.

--- source/prew/src/Data/BinCoord.cpp
+++ source/prew/src/Data/BinCoord.cpp
@@ -1,3 +1,4 @@
+#include <CppUtils/Num.h>
 #include <Data/BinCoord.h>
 
 #include <stdexcept>
@@ -31,9 +32,12 @@ const std::vector<double> &BinCoord::get_edge_up() const { return m_edge_up; }
 // Operators
 
 bool BinCoord::operator==(const BinCoord &other) const {
-  return (m_center == other.get_center()) &&
-         (m_edge_low == other.get_edge_low()) &&
-         (m_edge_up == other.get_edge_up());
+  /** Compare coordinates with 0.00001 precision (reasonable for most
+    *coordinates).
+   **/
+  return CppUtils::Num::equal_to_eps(m_center, other.get_center(), 1e-5) &&
+         CppUtils::Num::equal_to_eps(m_edge_low, other.get_edge_low(), 1e-5) &&
+         CppUtils::Num::equal_to_eps(m_edge_up, other.get_edge_up(), 1e-5);
 }
 
 bool BinCoord::operator!=(const BinCoord &other) const {
